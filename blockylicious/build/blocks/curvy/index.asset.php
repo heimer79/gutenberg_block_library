c256aba8d000adfab9a7171024a34d066bb03163
Heimer Martinez <hmartinez@cafetosoftware.com>
2024-06-12 13:40:26 -0500

    Set the blosck attributes using props from block json

--- blockylicious/build/blocks/curvy/index.asset.php
+++ blockylicious/build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '135f308c7b3057773273');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a68f031ae88de6616634');
